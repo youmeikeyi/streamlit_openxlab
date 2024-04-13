a6293f1cd3c5fb05976b8a89d6e093d9b495a6ea
Junco <xujinchao@sina.cn>
2024-04-13 13:30:43 +0800

    test build

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def main():
+    import streamlit as st
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+        - For more information on deploying Streamlit apps on the OpenXLab. [Official documentation](https://openxlab.org.cn/docs/en/apps/Streamlit%E5%BA%94%E7%94%A8.html)
+    """
+    )
+
+if __name__ == "__main__":
+    main()
